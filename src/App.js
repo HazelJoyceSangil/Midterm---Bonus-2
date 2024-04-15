e05b37fea0fda372449e45cf059db9862e625a36
Hazel-Joyce <hjysangil.student@ua.edu.ph>
2024-04-15 19:26:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import './App.css';
 
-
-
-function Sangil({display}) {
+function Sangil({ display }) {
   return (
-    <div className="Sangil">
+    <div className='Sangil'>
       {display}
     </div>
   );
 }
 
-
-function App() {
+function Sangil2() {
   return (
-    <div className="App">
-      <Sangil display={"hello  world"}/>
-     
+    <div className="Sangil2">
+      <div className="top">
+        <div className='topcolumn'>
+          <div class="item1">1</div>
+          <div class="item2">2</div>
+        </div>
+        <div class="item3">3</div>
+        <div class="item4">4</div>
+      </div>
+
+      <div className="bottom">
+        <div class="item5">5</div>
+        <div class="item6">6</div>
+        <div class="item7">7</div>
+        <div class="item8">8</div>
+      </div>
     </div>
   );
 }
 
-
-export default App;
+export default function App() {
+  return (
+    <div>
+      <Sangil display="Hazel Sangil"/>
+      <Sangil2/>
+    </div>
+  );
+}
